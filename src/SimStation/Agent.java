f684442368dea7f04033234a258926dd9a35e3e6
Josh <joshualawson8@gmail.com>
2021-04-07 10:12:59 -0700

    Simulation displays stats

--- src/SimStation/Agent.java
+++ src/SimStation/Agent.java
@@ -4,10 +4,25 @@ import java.io.Serializable;
 
 public class Agent implements Serializable, Runnable {
 
+    private String name;
+    //Heading is currentState?
+    private String heading;
+    private int xc;
+    private int yc;
+
+    private AgentState currentState;
+
 
     @Override
     public void run() {
 
+    }
+
+    public enum AgentState {
+        READY,
+        RUNNING,
+        SUSPENDED,
+        STOPPED
     }
 }
 
--- src/SimStation/Simulation.java
+++ src/SimStation/Simulation.java
@@ -1,16 +1,27 @@
 package SimStation;
 
 import mvc.Model;
+import tools.Utilities;
+
+import java.util.ArrayList;
 
 public class Simulation extends Model {
 
     private int clock = 0;
+    private ArrayList<Agent> Agents;
 
+    public Simulation(){
+        Agents = new ArrayList<Agent>();
+    }
 
+    public void changeState(String heading){
+        if(heading.equals("Stats")){
+            Utilities.inform(new String[]{"#Agents = "+Agents.size(),"clock = "+clock});
+        }
 
-    public void changeState(String heading){}
+    }
 
-    public Agent getNeighbor(){}
+    public Agent getNeighbor(){return null;}
 
     public void populate(){}
 
