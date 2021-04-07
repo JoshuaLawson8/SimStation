67d97935cda72ca519fdda6fa5f8997622cd2a06
Josh <joshualawson8@gmail.com>
2021-04-07 09:59:10 -0700

    Implementing Agent

---
+++ src/SimStation/Agent.java
@@ -0,0 +1,14 @@
+package SimStation;
+
+import java.io.Serializable;
+
+public class Agent implements Serializable, Runnable {
+
+
+    @Override
+    public void run() {
+
+    }
+}
+
+
--- src/SimStation/Simulation.java
+++ src/SimStation/Simulation.java
@@ -4,10 +4,17 @@ import mvc.Model;
 
 public class Simulation extends Model {
 
+    private int clock = 0;
 
 
 
     public void changeState(String heading){}
+
+    public Agent getNeighbor(){}
+
+    public void populate(){}
+
+
 
 }
 
