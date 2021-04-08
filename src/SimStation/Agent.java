bd77ca8002de0b852b8ccf19fa1d48331692cce8
Josh <joshualawson8@gmail.com>
2021-04-08 15:36:47 -0700

    Revising Agent methods

--- src/SimStation/Agent.java
+++ src/SimStation/Agent.java
@@ -5,34 +5,45 @@ import java.io.Serializable;
 public class Agent implements Serializable, Runnable {
 
     private String name;
-    //Heading is currentState?
     private String heading;
     private int xc;
     private int yc;
+    private Thread agentThread;
 
     private AgentState currentState;
 
+    public Agent(String name){
+        this.name = name;
+        currentState = AgentState.READY;
+    }
 
     @Override
     public void run() {
-        while(currentState.equals(AgentState.RUNNING)){
+        agentThread = Thread.currentThread();
+        while(!(currentState == AgentState.STOPPED)){
+            currentState = AgentState.RUNNING;
             update();
+            try{
+                Thread.sleep(100);
+                synchronized (this){
+                    while(currentState == AgentState.SUSPENDED){wait();}
+                }
+            }catch(Exception e){
+                System.out.println(e);
+            }
         }
     }
 
-    public void start(){
+    public synchronized void suspend(){ if(currentState == AgentState.RUNNING){ currentState = AgentState.SUSPENDED; }}
+    public synchronized void resume(){ if(!(currentState == AgentState.STOPPED)) notify(); }
+    public synchronized void stop(){ currentState = AgentState.STOPPED; }
+    public synchronized AgentState getState(){return currentState;}
+    public String getName(){return name;}
 
-    }
-
-    public void suspend(){
-
-    }
-
-    public void resume(){
-
-    }
-    public void stop(){
-
+    public synchronized void join() throws InterruptedException{
+        if(agentThread != null){
+            agentThread.join();
+        }
     }
 
     public void update(){
@@ -42,6 +53,7 @@ public class Agent implements Serializable, Runnable {
     public void move(int move){
 
     }
+
     public enum AgentState {
         READY,
         RUNNING,
