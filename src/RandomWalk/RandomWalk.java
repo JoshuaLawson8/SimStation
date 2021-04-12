a92bfa1a5a8e675a7dc066e6cc16961f098be928
Martin Vladimirov <martinvlad@MacBook-Pro-2.local>
2021-04-12 16:15:14 -0700

    added main method for random walk

--- src/RandomWalk/RandomWalk.java
+++ src/RandomWalk/RandomWalk.java
@@ -1,6 +1,8 @@
 package RandomWalk;
 
 import SimStation.Simulation;
+import SimStation.SimulationPanel;
+import mvc.AppPanel;
 
 public class RandomWalk extends Simulation {
 
@@ -11,6 +13,11 @@ public class RandomWalk extends Simulation {
         for(int i = 0; i < POP;){
             Agents.add(new Drunk("Drunk" + i));
         }
+
+
+    }
+    public static void main(String[]args){
+
 
     }
 }
