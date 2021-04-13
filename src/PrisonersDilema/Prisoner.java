ec4c249e31d471b1426d09460f5cd426ea74bfd9
Martin Vladimirov <martinvlad@MacBook-Pro-2.local>
2021-04-13 14:27:46 -0700

    added

--- src/PrisonersDilema/Prisoner.java
+++ src/PrisonersDilema/Prisoner.java
@@ -1,13 +1,34 @@
 package PrisonersDilema;
 
-public class Prisoner {
-    private int fitness = 0;
+import SimStation.Agent;
+import tools.Utilities;
 
-    public boolean cooperate(){
-       if(fitness > 0){
-           return true;
-       }
-       return false;
+import SimStation.Agent;
+
+public class Prisoner extends Agent {
+    private int fitness = 0;
+    public boolean cooperate() {
+      if(fitness > 0){
+          return true;
+      }
+      return false;
     }
 
+
+    public Prisoner(String name){
+        super(name);
+    }
+
+    @Override
+    public void update() {
+
+        //heading = Math.random();
+        xc = (int)(Math.random() * 10 + 1);
+        yc = (int)(Math.random() * 10 + 1);
+    }
+
+    @Override
+    public void move(int move) {
+
+    }
 }
